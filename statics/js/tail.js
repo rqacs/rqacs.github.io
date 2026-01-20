09c2e1f4f2aef3f2c6f1b27d4bb25b9c9d8176dc
Pranav Kapoor <pranavkapoorr@gmail.com>
2026-01-20 18:11:30 +0000

    test-commit

---
+++ statics/js/tail.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            primary:'#252529',
+            primary_hover: '#374151',
+            secondary: '#D1D5DB80',
+            secondary_hover: '#143f85',
+            light_color: '#f4f4f4',
+            nav:'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.5))',
+            nav_text:'#eee'
+        }
+      }
+    }
+  }
